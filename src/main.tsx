a6911e1a874892c2f2e7a6c1c4bb28d0af91d97d
Jae Young Seo <jaeyoungseo@jys.local>
2025-04-13 22:01:26 -0400

    adding analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./theme/theme";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 );
